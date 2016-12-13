7c1b99e93e1517f8673ea9706b9b11a9a6c7aa87
Dennis Li <dli357@gatech.edu>
2016-12-13 02:48:22 -0500

    Added a cover for the back of the game board

---
+++ Assets/Scripts/coverCubeCatchController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coverCubeCatchController : MonoBehaviour {
+    //Just to prevent the noMessageCatch error
+    //Keeps the mouseRay from going through the back
+    void mouseLeftClick() {
+        //No action on left click
+    }
+    void mouseRightClick() {
+        //No action on right click
+    }
+    void mouseOff() {
+        //No action on mouse off
+    }
+    void mouseOnLeft() {
+        //No action on mouse left down
+    }
+    void mouseOnRight() {
+        //No action on right mouse down
+    }
+    void mouseOut() {
+        //No action on mouse out
+    }
+    void mouseOver() {
+        //No action on mouse over
+    }
+}
