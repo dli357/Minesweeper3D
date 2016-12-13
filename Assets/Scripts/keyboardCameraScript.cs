1cadcb7fac0635f291db47eb5cb7c5f2224715a8
Dennis Li <dli357@gatech.edu>
2016-12-13 02:45:44 -0500

    Added additional controls for moving up, down, and faster

--- Assets/Scripts/keyboardCameraScript.cs
+++ Assets/Scripts/keyboardCameraScript.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class keyboardCameraScript : MonoBehaviour {
 
+    private int speed = 2;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,17 +12,28 @@ public class keyboardCameraScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+            speed = 5;
+        } else {
+            speed = 2;
+        }
         if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector3.forward);
+            transform.Translate(Vector3.forward * speed);
         }
         if (Input.GetKey(KeyCode.A)) {
-            transform.Translate(Vector3.left);
+            transform.Translate(Vector3.left * speed);
         }
         if (Input.GetKey(KeyCode.S)) {
-            transform.Translate(Vector3.back);
+            transform.Translate(Vector3.back * speed);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.Translate(Vector3.right);
+            transform.Translate(Vector3.right * speed);
+        }
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) {
+            transform.Translate(Vector3.down * speed);
+        }
+        if (Input.GetKey(KeyCode.Space)) {
+            transform.Translate(Vector3.up * speed);
         }
 	}
 }
